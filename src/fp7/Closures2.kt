f456acc0909ed22cf78552265eb98f4e0460832e
Vinoth Kumar <vinoth.mani2@wipro.com>
2017-12-26 15:48:02 +0530

    Functional Programming, Extension functions

---
+++ src/fp7/Closures2.kt
@@ -0,0 +1,25 @@
+package fp7
+
+fun outsideFn() {
+//    val num = 10
+
+    //Let there by unary operator that operates on this number
+    /*
+    Notice that, you did not pass 10 explicitly, but unaryOperation is able to access this
+    Allows me to access values outside of the lambda expressios that I'm actually declaring
+
+    The IDE says 'The value has been captured in a closure' .
+     */
+
+    for( num in 1..30) {
+        unaryOperation(20 , {x ->
+            println(num)
+            x * num
+        })
+    }
+
+}
+
+fun main(args: Array<String>) {
+
+}
